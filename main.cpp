9fafe8bae1c39d0829528f6eeecd405fc8c49f13
Nistiah <humbukland@gmail.com>
2021-02-01 12:59:14 +0100

    zmiana metody policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+int Policz(int a, int b){
+    return a+b;
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout<<"4853dfa143b6a8988a017f22bcc94d59a8b82910"<<std::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
     return 0;
 }
